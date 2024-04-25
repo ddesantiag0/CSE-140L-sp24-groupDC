fdafdb70935f7202d65590fe23faa877cf9e2871
David De Santiago <107174944+ddesantiag0@users.noreply.github.com>
2024-04-25 15:49:45 -0700

    Update mux5.sv

--- labs/lab1/Lab 1 Verilog starter code/mux5.sv
+++ labs/lab1/Lab 1 Verilog starter code/mux5.sv
@@ -3,7 +3,7 @@ module mux5 (input        d0, d1, d2, d3, d4,
              input [2:0]  s, 
              output logic y);
 
-always_comb begin   // always @(*)    // always @(d0, d1, d2, d3, d4, s);
+// always @(*)    // always @(d0, d1, d2, d3, d4, s);
 // fill in guts
 // when using always_comb, be sure to cover all cases & use =, not <=
 // case(s)
@@ -20,5 +20,15 @@ always_comb begin   // always @(*)    // always @(d0, d1, d2, d3, d4, s);
 //  5	   0
 //  6	   0
 //  7	   0	    s = 3'b111
+
+always_comb begin
+    case (s)
+        3'b000: y = d0;
+        3'b001: y = d1;
+        3'b010: y = d2;
+        3'b011: y = d3;
+        3'b100: y = d4;
+        default: y = 0;
+    endcase
 end
 endmodule
