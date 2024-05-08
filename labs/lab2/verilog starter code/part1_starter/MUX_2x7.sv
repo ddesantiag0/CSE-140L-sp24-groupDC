8d7675dd71f9646f69dffe73d3c0af12a5938dfc
David De Santiago <107174944+ddesantiag0@users.noreply.github.com>
2024-05-08 16:42:16 -0700

    Update MUX_2x7.sv

--- labs/lab2/verilog starter code/part1_starter/MUX_2x7.sv
+++ labs/lab2/verilog starter code/part1_starter/MUX_2x7.sv
@@ -1,13 +1,22 @@
-// MUX_2x7.sv
 module MUX_2x7(
-    input logic[6:0] input0_time,
-    input logic[6:0] input1_alarm,
-    input logic select,  // Selects between showing time or alarm
-    output logic[6:0] output_display
+    input logic[6:0] input0_a, // First input of the first pair
+    input logic[6:0] input1_a, // Second input of the first pair
+    input logic[6:0] input0_b, // First input of the second pair
+    input logic[6:0] input1_b, // Second input of the second pair
+    input logic select_a,      // Selector for the first pair
+    input logic select_b,      // Selector for the second pair
+    output logic[6:0] output_a, // Output for the first pair
+    output logic[6:0] output_b  // Output for the second pair
 );
 
+    // Implementing selection for the first pair
     always_comb begin
-        output_display = select ? input1_alarm : input0_time;
+        output_a = select_a ? input1_a : input0_a;
+    end
+
+    // Implementing selection for the second pair
+    always_comb begin
+        output_b = select_b ? input1_b : input0_b;
     end
 
 endmodule
