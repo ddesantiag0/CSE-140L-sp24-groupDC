87910a97f470065f51620fe84a860dc969f85807
David De Santiago <107174944+ddesantiag0@users.noreply.github.com>
2024-06-13 17:39:53 -0700

    Update top_level_5b_start.sv

--- labs/lab5/top_level_5b_start.sv
+++ labs/lab5/top_level_5b_start.sv
@@ -5,184 +5,189 @@ module top_level_5b(
   output logic   done);
 
 // memory interface
-  logic          wr_en;
-  logic    [7:0] raddr, 
-                 waddr,
-                 data_in;
-  logic    [7:0] data_out;             
+  logic          wr_en;                // data memory write enable
+  logic    [7:0] raddr,                // read address pointer
+                 waddr,                // write address pointer
+                 data_in;              // to dat_mem
+  logic    [7:0] data_out;             // from dat_mem
 
 // program counter
   logic[15:0] cycle_ct = 0;
 
 // LFSR interface
-  logic load_LFSR,
-        LFSR_en;
-  logic[5:0] LFSR_ptrn[6];           // the 6 possible maximal length LFSR patterns
+  logic load_LFSR,                     // copy taps and start into LFSR
+        LFSR_en;                       // 1: advance LFSR; 0: hold
+  logic[5:0] LFSR_ptrn[6];             // the 6 possible maximal length LFSR patterns
   assign LFSR_ptrn[0] = 6'h21;
   assign LFSR_ptrn[1] = 6'h2D;
   assign LFSR_ptrn[2] = 6'h30;
   assign LFSR_ptrn[3] = 6'h33;
   assign LFSR_ptrn[4] = 6'h36;
   assign LFSR_ptrn[5] = 6'h39;
-  logic[5:0] start;                  // LFSR starting state
+  logic[5:0] start;                    // LFSR starting state
   logic[5:0] LFSR_state[6];
-  logic[5:0] match;					 // got a match for LFSR (one hot)
-  logic[2:0] foundit;                // binary index equiv. of match
+  logic[5:0] match;                    // got a match for LFSR (one hot)
+  logic[2:0] foundit;                  // binary index equiv. of match
   int i;
+
+  logic[7:0] last_underscore_position = 'd8;
+  logic temp = 'd0;
 
 // instantiate submodules
 // data memory -- fill in the connections
-  dat_mem dm1(.clk(),.write_en(),.raddr(),.waddr(),
-       .data_in(),.data_out());                   // instantiate data memory
+  dat_mem dm1(
+    .clk(clk), 
+    .write_en(wr_en), 
+    .raddr(raddr), 
+    .waddr(waddr),
+    .data_in(data_in), 
+    .data_out(data_out)
+  );
 
-// 6 parallel LFSRs -- fill in the missing connections
-  lfsr6b l0(.clk() , 
-         .en   ()  ,            // 1: advance LFSR on rising clk
-         .init (),	            // 1: initialize LFSR
-         .taps (6'h21)     ,    // tap pattern 0
-         .start() ,	            // starting state for LFSR
-         .state(LFSR_state[0]));		   // LFSR state = LFSR output 
+// instantiate LFSR submodules
+  lfsr6b l0(
+    .clk(clk), 
+    .en(LFSR_en),                  // 1: advance LFSR on rising clk
+    .init(load_LFSR),              // 1: initialize LFSR
+    .taps(6'h21),                  // tap pattern 0
+    .start(start),                 // starting state for LFSR
+    .state(LFSR_state[0])
+  );
 
-  lfsr6b l1(... .taps(6'h2D),   .state(LFSR_state[1])
-/* fill in the guts: continue with other 5 lfsr6b
-*/
+  lfsr6b l1(
+    .clk(clk), 
+    .en(LFSR_en),                  // 1: advance LFSR on rising clk
+    .init(load_LFSR),              // 1: initialize LFSR
+    .taps(6'h2D),                  // tap pattern 1
+    .start(start),                 // starting state for LFSR
+    .state(LFSR_state[1])
+  );
 
+  lfsr6b l2(
+    .clk(clk), 
+    .en(LFSR_en),                  // 1: advance LFSR on rising clk
+    .init(load_LFSR),              // 1: initialize LFSR
+    .taps(6'h30),                  // tap pattern 2
+    .start(start),                 // starting state for LFSR
+    .state(LFSR_state[2])
+  );
 
-/* We need to advance the LFSR(s) once per clock cycle. 
-Same with raddr, waddr, since we can physically do one memory read and/or write
-per clock cycle. 
-*/
+  lfsr6b l3(
+    .clk(clk), 
+    .en(LFSR_en),                  // 1: advance LFSR on rising clk
+    .init(load_LFSR),              // 1: initialize LFSR
+    .taps(6'h33),                  // tap pattern 3
+    .start(start),                 // starting state for LFSR
+    .state(LFSR_state[3])
+  );
 
-// this block remaps a one-hot 6-bit code into a 3-bit binary count
-// acts like a priority encoder from MSB to LSB 
-  always_comb case(match)
-    6'b10_0000: foundit = 'd5;	    // because bit [5] was set
-    // fill in the guts
-	default: foundit = 0;           // covers bit[0] match and no match cases
-  endcase
+  lfsr6b l4(
+    .clk(clk), 
+    .en(LFSR_en),                  // 1: advance LFSR on rising clk
+    .init(load_LFSR),              // 1: initialize LFSR
+    .taps(6'h36),                  // tap pattern 4
+    .start(start),                 // starting state for LFSR
+    .state(LFSR_state[4])
+  );
 
-// program counter
-// as in Lab 4, you can do the whole lab without any branches or jumps
-  always @(posedge clk) begin  :clock_loop
+  lfsr6b l5(
+    .clk(clk), 
+    .en(LFSR_en),                  // 1: advance LFSR on rising clk
+    .init(load_LFSR),              // 1: initialize LFSR
+    .taps(6'h39),                  // tap pattern 5
+    .start(start),                 // starting state for LFSR
+    .state(LFSR_state[5])
+  );
+
+// priority encoder
+  always_comb begin
+    case(match)
+      6'b10_0000: foundit = 'd5;         // because bit [5] was set
+      6'b01_0000: foundit = 'd4;         // because bit [4] was set
+      6'b00_1000: foundit = 'd3;         // because bit [3] was set
+      6'b00_0100: foundit = 'd2;         // because bit [2] was set
+      6'b00_0010: foundit = 'd1;         // because bit [1] was set
+      default   : foundit =   0;         // covers bit[0] match and no match cases
+    endcase
+  end
+
+// program counter and main logic
+  always @(posedge clk) begin : clock_loop
     if(init) begin
       cycle_ct <= 'b0;
-	  match    <= 'b0;
-	end
-    else begin
+      match    <= 'b0;
+    end else begin
       cycle_ct <= cycle_ct + 1;
-	  if(cycle_ct== ) begin			// last symbol of preamble
-	    for(i=0; i<6; i++) begin
-	      match[i] <= ;				// which LFSR state conforms to our test bench LFSR? 
-		end
+
+      if (cycle_ct == 8) begin              // last symbol of preamble
+        for (i = 0; i < 6; i++) begin
+          match[i] <= ((data_out ^ 8'h5f) == {2'b00, LFSR_state[i]});   // which LFSR state conforms to our test bench LFSR? 
+        end
       end
     end
   end  
 
+// control logic
   always_comb begin 
-//defaults
+    // defaults
     load_LFSR = 'b0; 
     LFSR_en   = 'b0;   
-	wr_en     = 'b0;
-  case(cycle_ct)
-	0: begin 
-           raddr     = ;   // starting address for encrypted data to be loaded into device
-		   waddr     = ;   // starting address for storing decrypted results into data mem
-	     end		       // no op
-	1: begin 
-           load_LFSR = ;	  // initialize the 6 LFSRs
-           raddr     = ;
-		   waddr     = ;
-	     end		       // no op
-	2  : begin				   
-           LFSR_en   = ;	   // advance the 6 LFSRs     
-           raddr     = ;
-		   waddr     = ;
-         end
-	3  : begin			       // training seq.	-- run LFSRs & advance raddr
-	       LFSR_en = ;
-		   raddr     ;			  // advance raddr
-		   waddr = ;
-		 end
-	72  : begin
-            done = ;		// send acknowledge back to test bench to halt simulation
- 		    raddr =	;
- 		    waddr = ; 
-	     end
-	default: begin	         // covers cycle_ct 4-71
-	       LFSR_en = ;
-           raddr ; 
-           if(cycle_ct>) begin   // turn on write enable
-			 wr_en = ;
-			 if(cycle_ct>)		 // advance memory write address pointer
-			   waddr;
-		   end
-		   else begin
-		     waddr = ;
-			 wr_en = ;
-		   end
-//		   data_in = data_out^LFSR_state[foundit];
-	     end
-  endcase
-end
+    wr_en     = 'b0;
+    raddr     = 'b0;
+    waddr     = 'b0;
+    start     = 'b0;
+    done      = 'b0;
+    data_in   = 'b0;
 
-/*
-    if(!init && initQ) begin :init_loop  // falling init
-	  begin  :loop2			   
-        for(int jl=0;jl<7;jl++)
-	      LFSR[jl] =         dm1.core[64+jl][5:0]^6'h1f;
-          lfsr_trial[0][0] = dm1.core[64][5:0]^6'h1f;
-          lfsr_trial[1][0] = dm1.core[64][5:0]^6'h1f;
-          lfsr_trial[2][0] = dm1.core[64][5:0]^6'h1f;
-          lfsr_trial[3][0] = dm1.core[64][5:0]^6'h1f;
-          lfsr_trial[4][0] = dm1.core[64][5:0]^6'h1f;
-          lfsr_trial[5][0] = dm1.core[64][5:0]^6'h1f;
-//          $display("trial 0 = %h",lfsr_trial[0][0]);
-          for(int kl=0;kl<6;kl++) begin :trial_loop
-            lfsr_trial[0][kl+1] = (lfsr_trial[0][kl]<<1)+(^(lfsr_trial[0][kl]&LFSR_ptrn[0]));   
-            lfsr_trial[1][kl+1] = (lfsr_trial[1][kl]<<1)+(^(lfsr_trial[1][kl]&LFSR_ptrn[1]));   
-            lfsr_trial[2][kl+1] = (lfsr_trial[2][kl]<<1)+(^(lfsr_trial[2][kl]&LFSR_ptrn[2]));   
-            lfsr_trial[3][kl+1] = (lfsr_trial[3][kl]<<1)+(^(lfsr_trial[3][kl]&LFSR_ptrn[3]));   
-            lfsr_trial[4][kl+1] = (lfsr_trial[4][kl]<<1)+(^(lfsr_trial[4][kl]&LFSR_ptrn[4]));   
-            lfsr_trial[5][kl+1] = (lfsr_trial[5][kl]<<1)+(^(lfsr_trial[5][kl]&LFSR_ptrn[5]));   
-            $display("trials %d %h %h %h %h %h %h    %h",  kl,
-				 lfsr_trial[0][kl+1],
-				 lfsr_trial[1][kl+1],
-				 lfsr_trial[2][kl+1],
-				 lfsr_trial[3][kl+1],
-				 lfsr_trial[4][kl+1],
-				 lfsr_trial[5][kl+1],
-				 LFSR[kl+1]);			  
-          end :trial_loop
-		  for(int mm=0;mm<6;mm++) begin :ureka_loop
-            $display("mm = %d  lfsr_trial[mm] = %h, LFSR[6] = %h",
-			     mm, lfsr_trial[mm][6], LFSR[6]); 
-		    if(lfsr_trial[mm][6] == LFSR[6]) begin
-			  foundit = mm;
-			  $display("foundit = %d LFSR[6] = %h",foundit,LFSR[6]);
+    case(cycle_ct)
+      0: begin 
+        raddr     = 64;                // starting address for encrypted data to be loaded into device
+        waddr     = 'd0;               // starting address for storing decrypted results into data mem
+      end                              // no op
+      1: begin 
+        load_LFSR = 'b1;               // initialize the 6 LFSRs
+        raddr     = 64;
+        start     = 8'h5f ^ data_out;
+        waddr     = 'd0;
+        LFSR_en   = 'b1;
+      end                              // no op
+      2: begin         
+        LFSR_en   = 'b1;               // advance the 6 LFSRs     
+        raddr     = 64;
+        waddr     = 'd0;
+      end
+      3: begin                         // training seq. -- run LFSRs & advance raddr
+        LFSR_en   = 'b1;
+        raddr     = 64 + cycle_ct - 2; // advance raddr
+        waddr     = 'd0;
+      end
+      72: begin
+        done      = 'b1;               // send acknowledge back to test bench to halt simulation
+        raddr     = 'd0;
+        waddr     = 'd0; 
+      end
+      default: begin                   // covers cycle_ct 4-71
+        if(cycle_ct <= 8) begin         // turn on write enable
+          LFSR_en = 'b1;
+          raddr   = 64 + cycle_ct - 2;
+          waddr   = 'd0;
+        end else begin
+          LFSR_en = 'b1;
+          raddr   = 64 + cycle_ct - 2;
+          data_in = data_out ^ {2'b00, LFSR_state[foundit]};
+
+          if (last_underscore_position == cycle_ct - 1) begin
+            if (data_in == 8'h5f) begin
+              temp = 'b1;
             end
-		  end :ureka_loop
-		  $display("foundit fer sure = %d",foundit);								   
-		  for(int jm=0;jm<63;jm++)
-		    LFSR[jm+1] = (LFSR[jm]<<1)+(^(LFSR[jm]&LFSR_ptrn[foundit]));
-          for(int mn=7;mn<64-7;mn++) begin  :first_core_write
-		    dm1.core[mn-7] = dm1.core[64+mn-7]^{2'b0,LFSR[mn-7]};
-			$display("%dth core = %h LFSR = %h",mn,dm1.core[64+mn-7],LFSR[mn-7]);
-          end   :first_core_write
-         #10ns;
-         for(km=0; km<64; km++) begin
-            if(dm1.core[km]==8'h5f) continue;
-            else break;  
-          end     
-          $display("underscores to %d th",km);
-          for(int kl=0; kl<64; kl++) begin
-            dm1.core[kl] = dm1.core[kl+km];
-		    $display("%dth core = %h",kl,dm1.core[kl]);
           end
-	  end   :loop2
-    end :init_loop
-  end  :clock_loop
 
-  always_comb
-    done = &cycle_ct[6:0];   // holds for two clocks
-*/
+          if (last_underscore_position != cycle_ct) begin
+            wr_en = 'b1;
+            waddr = cycle_ct - last_underscore_position - 1;
+          end  
+        end
+      end
+    endcase
+  end
 endmodule
